91aadbb825993b3005a53c40d4dd88915b0973a0
ahmedctstrainer <ahmedctstrainer@gmail.com>
2023-02-19 12:35:48 +0530

    js files

---
+++ 1.js
@@ -0,0 +1,19 @@
+
+
+function doTask(){
+// simple hello message
+document.write('Hello Java script');
+document.writeln('Welcome to javaScript<br>');
+document.writeln('This is JavaScript Class<br>');
+
+/* write a a para with
+red color
+on html 
+document 
+*/
+document.writeln('<p style="color:red;">Welcome to javaScript</p>');
+document.writeln("<br>");
+
+}
+
+
